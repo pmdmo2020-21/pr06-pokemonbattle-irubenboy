6ff8f33ab1ee83a7588efb7699efecb98da7b173
Rubeb <rubenmoreno.saladillo@gmail.com>
2020-11-08 18:56:33 +0100

    Desarrollo del Proyecto
    Se ha desarrollado el proyecto de Pokemons

--- app/src/main/java/es/iessaladillo/pedrojoya/intents/data/local/model/Pokemon.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/intents/data/local/model/Pokemon.kt
@@ -2,13 +2,16 @@ package es.iessaladillo.pedrojoya.intents.data.local.model
 
 
 class Pokemon(
-    id: Int,
-    name: String,
-    strength: Double,
-    photo: Int){
+        val id: Long,
+        var name: String,
+        private val strength: Double,
+        val photo: Int){
 
-    val id: Int = id
-    val name: String = name
-    val strength: Double = strength
-    val photo: Int = photo
+    fun stronger(p: Pokemon): Pokemon{ // Función que devuelve el pokemón más fuerte, comparando dos pokemons
+        return if(strength - p.strength >= 0){
+            this
+        } else {
+            p
+        }
+    }
 }
